e458a6f5101982f35cb3064c34c14f93a477f11e
masashi takeshita <masashibox@gmail.com>
2019-10-15 01:07:41 +0900

    Section12-106

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
+
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 
 class InputPage extends StatefulWidget {
   @override
@@ -12,15 +20,51 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(accentColor: Colors.purple),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child:
+                  ReusableCard(
+                    color: activeCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE'
+                    ),
+                  )
+                ),
+                Expanded(child:
+                  ReusableCard(
+                    color: activeCardColor,
+                    cardChild: IconContent(
+                        icon: FontAwesomeIcons.venus,
+                        label: 'FEMALE'
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(color: activeCardColor)
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: ReusableCard(color: activeCardColor)),
+                Expanded(child: ReusableCard(color: activeCardColor)),
+              ],
+            ),
+          ),
+          Container(
+            color: bottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
+        ],
+      )
     );
   }
 }
