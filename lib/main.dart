3700ad44f18eb7c101040dd00df5049fc96624a4
竹下雅 <takeshita@dentalight.co.jp>
2019-10-21 02:09:29 +0900

    Section12-112

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'input_page.dart';
 import 'package:flutter/material.dart';
 
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -16,4 +16,3 @@ class BMICalculator extends StatelessWidget {
     );
   }
 }
-
